dc0363d650140d209953fa3224f561d5d2729bc8
Markus Duft <mduft@gentoo.org>
2011-04-07 23:09:35 +0200

    added some minor bits to threading.

--- src/thread.h
+++ src/thread.h
@@ -6,9 +6,22 @@
 #include "tachyon.h"
 
 /**
+ * Describes the different states a thread can be in.
+ */
+typedef enum {
+    Initial,    /**< initial state while creating the thread. */
+    Runnable,   /**< state while thread is waiting to run */
+    Running,    /**< state while thread is running */
+    Waiting,    /**< thread is waiting for something */
+    Done        /**< thread has exited, waiting for cleanup */
+} thread_state_t;
+
+/**
  * The descriptor of a thread. Holds all relevant
  * information associated with it.
  */
 typedef struct {
+    thread_state_t;
+    void* stack;
 } thread_t;
 
